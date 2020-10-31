98ebdf7f0bffa88c598a2085172dde21552f9c97
Hyeongho <rlaguddgh@nate.com>
2020-11-01 02:38:40 +0900

    1101

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Platform, TouchableHighlight, Button } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Platform, TouchableHighlight, Button, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { List, DataTable } from 'react-native-paper'
+import { List, DataTable, Checkbox, Dialog, Portal } from 'react-native-paper'
 
 function HomeScreen({navigation})
 {
@@ -54,18 +54,87 @@ function CityScreen() {
   }
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-       <Button onPress = {pressCity} title = "도시"/>
-    </View>
+    <View style = {{flex: 1, justifyContent: 'flex-end'}}>
+    <List.Accordion>
+      <DataTable style = {{backgroundColor: 'white'}}>
+      <DataTable.Row>
+      <DataTable.Cell>시장</DataTable.Cell>
+      <DataTable.Cell>진우</DataTable.Cell>
+
+    </DataTable.Row>
+      <DataTable.Row>
+      <DataTable.Cell>도시 등급</DataTable.Cell>
+      <DataTable.Cell>소도시</DataTable.Cell>
+
+    </DataTable.Row>
+
+    <DataTable.Row>
+      <DataTable.Cell>보유자원</DataTable.Cell>
+      <DataTable.Cell>73502</DataTable.Cell>
+
+    </DataTable.Row>
+
+    <DataTable.Row>
+      <DataTable.Cell>인구수</DataTable.Cell>
+      <DataTable.Cell>3502</DataTable.Cell>
+
+    </DataTable.Row>
+
+      </DataTable>
+    </List.Accordion>
+  </View>
   );
 }
 
 function AntScreen() {
+  const [checked, setChecked] = React.useState(false); 
+
+  const [visible, setVisible] = React.useState(false);
+
+  const showDialog = () =>
+  {
+    <Portal>
+        <Dialog visible={visible} onDismiss={hideDialog}>
+          <Dialog.Title>Alert</Dialog.Title>
+          <Dialog.Content>
+            <Paragraph>This is simple dialog</Paragraph>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={hideDialog}>Done</Button>
+          </Dialog.Actions>
+        </Dialog>
+    </Portal>
+  }
+  const hideDialog = () => setVisible(false);
+
   return (
     <View style = {{flex: 1, justifyContent: 'flex-end'}}>
       <List.Accordion>
-        <DataTable style = {{backgroundColor: 'green'}}>
-          <Button title = 'store' />
+        <DataTable style = {{backgroundColor: 'white'}}>
+          <DataTable.Row onPress = {showDialog}>
+          <Image source = {require('./assets/c625dcdf7f1fb4d6.png')}/>
+            <DataTable.Cell>  일꾼개미</DataTable.Cell>
+            <DataTable.Cell numeric>20자원</DataTable.Cell>
+          </DataTable.Row>
+
+          <DataTable.Row>
+          <Image source = {require('./assets/c625dcdf7f1fb4d6.png')} />
+            <DataTable.Cell>  감독관개미</DataTable.Cell>
+            <DataTable.Cell numeric>40자원</DataTable.Cell>
+          </DataTable.Row>
+
+          <DataTable.Row>
+          <Image source = {require('./assets/c625dcdf7f1fb4d6.png')}/>
+            <DataTable.Cell>  회사원개미</DataTable.Cell>
+            <DataTable.Cell numeric>80자원</DataTable.Cell>
+          </DataTable.Row>
+
+          <DataTable.Row>
+          <Image source = {require('./assets/c625dcdf7f1fb4d6.png')}/>
+            <DataTable.Cell>  회장개미</DataTable.Cell>
+            <DataTable.Cell numeric>200자원</DataTable.Cell>
+          </DataTable.Row>
+
         </DataTable>
       </List.Accordion>
     </View>
@@ -74,16 +143,66 @@ function AntScreen() {
 
 function BuildingScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>건물</Text>
+    <View style = {{flex: 1, justifyContent: 'flex-end'}}>
+      <List.Accordion>
+        <DataTable style = {{backgroundColor: 'white'}}>
+          <DataTable.Row>
+          <Image source = {require('./assets/07be15af8c4cd374.png')}/>
+            <DataTable.Cell>  벽돌집</DataTable.Cell>
+            <DataTable.Cell numeric>20자원</DataTable.Cell>
+          </DataTable.Row>
+
+          <DataTable.Row>
+          <Image source = {require('./assets/07be15af8c4cd374.png')} />
+            <DataTable.Cell>  아파트</DataTable.Cell>
+            <DataTable.Cell numeric>40자원</DataTable.Cell>
+          </DataTable.Row>
+
+          <DataTable.Row>
+          <Image source = {require('./assets/07be15af8c4cd374.png')} />
+            <DataTable.Cell>  개미 회사</DataTable.Cell>
+            <DataTable.Cell numeric>80자원</DataTable.Cell>
+          </DataTable.Row>
+
+          <DataTable.Row>
+          <Image source = {require('./assets/07be15af8c4cd374.png')} />
+            <DataTable.Cell>  개미 호텔</DataTable.Cell>
+            <DataTable.Cell numeric>200자원</DataTable.Cell>
+          </DataTable.Row>
+
+        </DataTable>
+      </List.Accordion>
     </View>
   );
 }
 
 function MapScreen() {
   return (
-    <View>
-      <Text>지도</Text>
+    // <View style = {{flex: 1, justifyContent: 'flex-end'}}>
+    //   <List.Accordion>
+    //     <View style = {{backgroundColor: 'green'}}>
+    //       <View style = {{width: 100}}>
+    //         <Button title = 'store' style = {{justifyContent: 'flex-start'}} />
+    //         <Button title = 'store' style = {{justifyContent: 'flex-start'}} />
+    //         <Button title = 'store' style = {{justifyContent: 'flex-start'}} />
+    //       </View>
+    //     </View>
+    //   </List.Accordion>
+    // </View>
+
+    <View style = {{flex: 1, justifyContent: 'flex-end'}}>
+      <List.Accordion>
+      <DataTable style = {{backgroundColor: 'red'}}>
+
+
+        <Button title = 'store' />
+        <Button title = '업적' />
+        <Button title = '수집품' />
+        <Button title = '확장' />
+
+ 
+      </DataTable>
+    </List.Accordion>
     </View>
   );
 }
@@ -98,7 +217,7 @@ export default function App() {
           <Stack.Screen name = "Home" component = {HomeScreen}></Stack.Screen>
           <Stack.Screen name = "Main" component = {MainScreen}></Stack.Screen>
         </Stack.Navigator>
-      </NavigationContainer>
+    </NavigationContainer>
   );
 }
 
